4e824dda755a324b9d170cf33bdbb7b57677fea8
Enes Şahin <Enes ahin@DESKTOP-DMMEA5V.home>
2025-12-16 00:15:13 +0300

    denek

--- CasinoBack/Main.java
+++ CasinoBack/Main.java
@@ -1,11 +1,17 @@
 
-public class Main {
-
+public class Main extends Thread {
+public void run() {
+	int a=0;
+	while(a<5) {
+		System.out.println("hi");
+		a++;
+	}
+}
 	public static void main(String[] args) {
-		System.out.println("fghdhdghfdgfh");
-		int a =5; 
-		System.out.println("cevap;"+a/5);
-		System.out.println("değişiklik yapıldı");
+		Main t= new Main();
+		t.start();
+		for(int a=0;a<1000;a++) {System.out.println(a);}
+	
 
 	}
 
